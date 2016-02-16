e0a5b7d977e0860779bd851017113ad0be023be3
Jorge Bernal <jbernal@gmail.com>
2016-02-16 19:03:33 +0100

    Showing a plan

--- data-flow-storekit-products.swift
+++ data-flow-storekit-products.swift
@@ -1,17 +1,21 @@
 import UIKit
 import StoreKit
 
-class PlansListViewController: UITableViewController, SKProductsRequestDelegate {
-    // ...
-
+class StoreService: NSObject, SKProductsRequestDelegate {
+    typealias Completion = [SKProduct] -> Void
     let identifiers = Set([ "com.wordpress.test.premium.1year", "com.wordpress.test.business.1year"])
+    static let sharedInstance = StoreService()
 
     var request: SKProductsRequest? = nil
+    var products: [SKProduct]? = nil
+    var completionBlocks = [Completion]()
 
-    deinit {
-        if let request = self.request {
-            request.delegate = nil
-            request.cancel()
+    func getProducts(completion: [SKProduct] -> Void) {
+        if let products = self.products {
+            completion(products)
+        } else {
+            completionBlocks.append(completion)
+            fetchProducts()
         }
     }
 
@@ -26,8 +30,32 @@ class PlansListViewController: UITableViewController, SKProductsRequestDelegate 
     }
 
     func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
-        let products = response.products
-        // Reload data showing products
+        products = response.products
+        for completion in completionBlocks {
+            completion(response.products)
+        }
+        completionBlocks.removeAll()
+    }
+
+}
+
+class PlansListViewController: UITableViewController {
+    // ...
+
+    func fetchProducts() {
+        StoreService.sharedInstance.getProducts { products in
+            // Reload data showing products
+        }
+    }
+}
+
+class PlanDetailsViewController: UITableViewController {
+    // ...
+
+    func fetchProducts() {
+        StoreService.sharedInstance.getProducts { products in
+            // Reload data showing products
+        }
     }
 }
 
