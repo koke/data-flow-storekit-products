aee61992ad52b6376232d37247b0455e677bd264
Jorge Bernal <jbernal@gmail.com>
2016-02-16 19:02:55 +0100

    Add canceling on deinit

--- data-flow-storekit-products.swift
+++ data-flow-storekit-products.swift
@@ -1,13 +1,28 @@
 import UIKit
 import StoreKit
 
-class PlanListViewController: UITableViewController, SKProductsRequestDelegate {
+class PlansListViewController: UITableViewController, SKProductsRequestDelegate {
     // ...
+
     let identifiers = Set([ "com.wordpress.test.premium.1year", "com.wordpress.test.business.1year"])
 
+    var request: SKProductsRequest? = nil
+
+    deinit {
+        if let request = self.request {
+            request.delegate = nil
+            request.cancel()
+        }
+    }
+
     func fetchProducts() {
+        guard self.request == nil else {
+            return
+        }
         let request = SKProductsRequest(productIdentifiers: identifiers)
+        request.delegate = self
         request.start()
+        self.request = request
     }
 
     func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
